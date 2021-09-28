b3f115cf16eb8a3d0c4661c4a0f5a2dd0d7c2c23
Hagen @ Bristol <dog@k9design.co.uk>
2021-09-28 14:19:57 +0100

    exercise 2 done, cleanup on exercise 1 + eslint breaking bug found in exercise 7

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -9,27 +9,34 @@ function capitalize(word) {
   //return word.substring(0, 1).toUpperCase()+word.substring(1, word.length);
 
   // which one is most readable?
-  
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
   // Add your code here!
-  return firstName.substring(0, 1).toUpperCase() + "." + lastName.substring(0, 1).toUpperCase()
+  return (
+    firstName.substring(0, 1).toUpperCase() +
+    "." +
+    lastName.substring(0, 1).toUpperCase()
+  );
 }
 
 // fairly accurate rounding algorithm
-function roundNumber(num, scale) {
-  if(!("" + num).includes("e")) {
-    return +(Math.round(num + "e+" + scale)  + "e-" + scale);
+function roundNumberWithDecimals(num, scale) {
+  if (!("" + num).includes("e")) {
+    return +(Math.round(num + "e+" + scale) + "e-" + scale);
   } else {
     var arr = ("" + num).split("e");
-    var sig = ""
-    if(+arr[1] + scale > 0) {
+    var sig = "";
+    if (+arr[1] + scale > 0) {
       sig = "+";
     }
-    return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
+    return +(
+      Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) +
+      "e-" +
+      scale
+    );
   }
 }
 
@@ -37,32 +44,39 @@ function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   // Add your code here!
-  return roundNumber( originalPrice + ((originalPrice/100)*vatRate) , 2);
+  return roundNumberWithDecimals(
+    originalPrice + (originalPrice / 100) * vatRate,
+    2
+  );
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  return roundNumber( originalPrice - ((originalPrice/100)*reduction) , 2);
+  return roundNumberWithDecimals(
+    originalPrice - (originalPrice / 100) * reduction,
+    2
+  );
   // Add your code here!
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
   // Add your code here!
-  const midNum = str.length/2;
-  const midNumFloored = Math.floor(midNum);
-  if(midNum == midNumFloored) { // even
-    return str.substring(midNum-1, midNum+1);
-  }else{ // odd
-    return str.substring(midNumFloored, midNumFloored+1);
+  let midNum = str.length / 2;
+  let midNumFloored = Math.floor(midNum);
+  if (midNum == midNumFloored) {
+    // even
+    return str.substring(midNum - 1, midNum + 1);
+  } else {
+    // odd
+    return str.substring(midNumFloored, midNumFloored + 1);
   }
 }
 
 function reverseString(str) {
-  return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+  return str === "" ? "" : reverseString(str.substr(1)) + str.charAt(0);
 }
-
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
@@ -74,13 +88,13 @@ function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Add your code here!
   var reversedWords = [];
-  words.forEach(word => reversedWords.push(reverseString(word)));
-  return reversedWords
+  words.forEach((word) => reversedWords.push(reverseString(word)));
+  return reversedWords;
 }
 
 function filterByType(item) {
   if (item.type == "Linux") {
-    return true
+    return true;
   }
   return false;
 }
@@ -88,24 +102,24 @@ function filterByType(item) {
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
   // Add your code here!
-  return (users.filter(filterByType)).length
+  return users.filter(filterByType).length;
 }
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
   // Add your code here!
   var total = 0;
-  scores.forEach(score => total += score);
-  return roundNumber(total/scores.length, 2)
+  scores.forEach((score) => (total += score));
+  return roundNumberWithDecimals(total / scores.length, 2);
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
   // Add your code here!
-  if(n%3 == 0 && n%5 == 0) return "fizzbuzz"
-  if(n%3 == 0) return "fizz"
-  if(n%5 == 0) return "buzz"
-  return n
+  if (n % 3 == 0 && n % 5 == 0) return "fizzbuzz";
+  if (n % 3 == 0) return "fizz";
+  if (n % 5 == 0) return "buzz";
+  return n;
 }
 
 module.exports = {
@@ -118,5 +132,5 @@ module.exports = {
   reverseAllWords,
   countLinuxUsers,
   getMeanScore,
-  simpleFizzBuzz
+  simpleFizzBuzz,
 };
--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,37 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  return person.city === "Manchester";
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  return people === 0 ? 0 : Math.ceil(people / 40);
+}
+
+function filterBySheep(item) {
+  return item.toLowerCase() == "sheep" ? true : false;
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+
+  return arr.filter(filterBySheep).length;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  let postcode = person.address.postCode;
+  return postcode.charAt(0) === "M" && !isNaN(postcode.charAt(1));
 }
 
 module.exports = {
@@ -28,5 +39,5 @@ module.exports = {
   isFromManchester,
   countSheep,
   getBusNumbers,
-  hasMPostCode
+  hasMPostCode,
 };

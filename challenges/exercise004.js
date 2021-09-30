13964c1556332ecef7d24354fe2688245d654565
Hagen @ Bristol <dog@k9design.co.uk>
2021-09-30 12:23:27 +0100

    exercise 4 with filter, map and reduce

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -18,87 +18,51 @@ function roundNumberWithDecimals(num, scale) {
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
-  let smallNumsArray = [];
-  nums.forEach((n) => {
-    if (n < 1) smallNumsArray.push(n);
-  });
-  return smallNumsArray;
+  return nums.filter( n => n < 1);
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
   // Your code here
-  let namesResults = [];
-  names.forEach((w) => {
-    if (w.substr(0, char.length) == char) namesResults.push(w);
-  });
-  return namesResults;
+  return names.filter(w => w.substr(0, char.length) == char)
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
   // Your code here
-  let wordsResults = [];
-  words.forEach((w) => {
-    if (w.substr(0, 3) == "to ") wordsResults.push(w);
-  });
-  return wordsResults;
+  return words.filter(w => w.substr(0, 3) == "to ")
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
-  let numResults = [];
-  nums.forEach((n) => {
-    if (n / Math.round(n) == 1) numResults.push(n);
-  });
-  return numResults;
+  return nums.filter(n => n / Math.round(n) == 1)
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
   // Your code here
-  let cityResults = [];
-  users.forEach((u) => {
-    cityResults.push(u.data.city.displayName);
-  });
-  return cityResults;
+  return users.map(u => u.data.city.displayName)
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
-  let numResults = [];
-  nums.forEach((n) => {
-    numResults.push( roundNumberWithDecimals( Math.sqrt(n) ,2) );
-  });
-  return numResults;
+  return nums.map(n => roundNumberWithDecimals( Math.sqrt(n) ,2) )
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
   // Your code here
-  let wordsResults = [];
-  sentences.forEach((w) => {
-    if (w.toLowerCase().indexOf(str.toLowerCase()) > -1) wordsResults.push(w);
-  });
-  return wordsResults;
+  return sentences.filter(w => w.toLowerCase().indexOf(str.toLowerCase()) > -1)
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
   // Your code here
-  let numResults = [];
-  triangles.forEach((arr) => {
-    let highestNumber = 0;
-    arr.forEach((n2) => {
-      if (n2 > highestNumber) highestNumber = n2;
-    });
-    numResults.push(highestNumber);
-  });
-  return numResults;
+  return triangles.map(n => n.reduce((prev, cur) => cur > prev ? cur : prev , 0))
 }
 
 module.exports = {

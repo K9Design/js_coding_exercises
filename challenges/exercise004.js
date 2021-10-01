127283d966692ed7d53e9771b7056fa361dbb8da
Hagen @ Bristol <dog@k9design.co.uk>
2021-10-01 10:23:15 +0100

    exercise 5 wip

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -18,51 +18,51 @@ function roundNumberWithDecimals(num, scale) {
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
-  return nums.filter( n => n < 1);
+  return nums.filter((n) => n < 1);
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
   // Your code here
-  return names.filter(w => w.substr(0, char.length) == char)
+  return names.filter((w) => w.substr(0, char.length) == char);
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
   // Your code here
-  return words.filter(w => w.substr(0, 3) == "to ")
+  return words.filter((w) => w.substr(0, 3) == "to ");
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
-  return nums.filter(n => n / Math.round(n) == 1)
+  return nums.filter((n) => n / Math.round(n) == 1);
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
   // Your code here
-  return users.map(u => u.data.city.displayName)
+  return users.map((u) => u.data.city.displayName);
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
-  return nums.map(n => roundNumberWithDecimals( Math.sqrt(n) ,2) )
+  return nums.map((n) => roundNumberWithDecimals(Math.sqrt(n), 2));
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
   // Your code here
-  return sentences.filter(w => w.toLowerCase().indexOf(str.toLowerCase()) > -1)
+  return sentences.filter((w) => w.toLowerCase().indexOf(str.toLowerCase()) > -1);
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
   // Your code here
-  return triangles.map(n => n.reduce((prev, cur) => cur > prev ? cur : prev , 0))
+  return triangles.map((n) => n.reduce((prev, cur) => (cur > prev ? cur : prev), 0));
 }
 
 module.exports = {
--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -2,26 +2,45 @@ const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
   // Your code here!
+  let position = nums.indexOf(n);
+  if(position === -1 || position === nums.length-1) return null; // number doesnt exist
+  return nums[position+1];
 };
+
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
   // Your code here!
+  let object = {'0':0, '1':0};
+  str.split("").forEach(s => object[s]++ )
+  return object
+
 };
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
   // Your code here!
+  let reverse = "";
+  String(n).split("").forEach(c => reverse = c + reverse)
+  return Number(reverse)
 };
 
 const sumArrays = arrs => {
   if (arrs === undefined) throw new Error("arrs is required");
   // Your code here!
+  let sum = 0;
+  arrs.forEach(a => a.forEach( n => sum += n));
+  return sum
 };
 
 const arrShift = arr => {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  if(arr.length < 2) return arr
+  let ar = arr.slice(-1).concat(arr.slice(1, -1) , arr.slice(0,1));
+  console.log(ar);
+  return ar
+  
 };
 
 const findNeedle = (haystack, searchTerm) => {
